c16d96c7cfa58747470bd2da1dc6f66b3282b2b6
a-pompom <aoi.matsuda.3x@gmail.com>
2020-02-24 15:33:44 +0900

    TOP画面用のカテゴリ一覧取得APIの作成
    GETリクエストによってDBに登録されたカテゴリ一覧を取得するAPIを作成
    これにより、TOP画面で表示されるカテゴリ一覧をDBに登録されたもので動的に切り替え可能となった
    
    #3

--- src/command/urls.py
+++ src/command/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views.topView import TopView
 
 urlpatterns = [
-    path('', TopView.as_view(), name='top')
+    path('category/', TopView.as_view(), name='top')
 ]
